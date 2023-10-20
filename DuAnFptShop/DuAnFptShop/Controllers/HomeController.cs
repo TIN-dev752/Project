3ed183087f256af95ee67c5c8424705bd985c05d
nguyenduythang139 <nguyenduythang1392004@gmail.com>
2023-10-20 19:29:02 +0700

    first commit

---
+++ DuAnFptShop/DuAnFptShop/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DuAnFptShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
+        public ActionResult ProductCategory()
+        {
+            return View();
+        }
+        public ActionResult ShopCart()
+        {
+            return View();
+        }
+       
+    }
+}
